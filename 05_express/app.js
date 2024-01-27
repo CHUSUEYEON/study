4d394a68d1ffa14073c283944c234df88c1976d7
CHUSUEYEON <rmffkfk226@naver.com>
2024-01-28 00:23:43 +0900

    study

--- 05_express/app.js
+++ 05_express/app.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/", function (req, res) {
-  // res.send("hello express");
+  console.log(req.query);
   res.render("index");
+  //   res.send("hihi");
 });
 
 app.get("/getForm", function (req, res) {
   res.render("result", {
     userInfo: req.query,
+    addInfo: false,
+  });
+  console.log(req.query);
+});
+
+app.post("/", function (req, res) {
+  res.render("index");
+  console.log(req.body);
+});
+
+app.post("/postForm", function (req, res) {
+  console.log(req.body);
+  const postInfo = req.body;
+  res.render("result", {
+    userInfo: postInfo,
+    addInfo: true,
   });
 });
 
-app.listen(PORT, function (req, res) {
+app.listen(PORT, function () {
   console.log(`http://localhost:${PORT}`);
 });
